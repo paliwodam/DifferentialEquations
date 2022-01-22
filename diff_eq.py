3195986769afa71a4fd2451fbdcb9f3a8604ba3a
Martyna Paliwoda <martynapaliwoda.2001@gmail.com>
2022-01-22 15:16:40 +0100

    Finished

---
+++ diff_eq.py
@@ -0,0 +1,60 @@
+from numpy.linalg import inv
+from functools import partial
+from math import sin
+
+import scipy.integrate as integrate
+import numpy as np
+
+a = 0
+b = 2
+
+
+def B(u, v):
+    return v(b) * u(b) + integrate.quad(lambda x: -v(x) * u(x), a, b)[0]
+
+
+def L(v):
+    return integrate.quad(lambda x: sin(x) * v(x), a, b)[0]
+
+
+def f_linear(a, b):
+    return lambda x: a * x + b
+
+
+def get_e(n):
+    e = []
+    for i in range(1, n + 2):
+        a = n + 1
+        b = 1 - i
+        e.append(partial(f_linear(a, b)))
+
+    return e
+
+
+def get_arrays(B, L, e):
+    a_B = np.array([[B(u, v) for u in e] for v in e])
+    a_L = np.array([L(v) for v in e])
+
+    return a_B, a_L
+
+
+def get_a_u(a_B, a_L):
+    return inv(a_B).dot(a_L)
+
+
+def union_f_g(f, g):
+    return lambda x: f(x) + g(x)
+
+
+def solve(n):
+    e = get_e(n)
+    e_ = e[0:n]
+    a_B, a_L = get_arrays(B, L, e_)
+    a_u = get_a_u(a_B, a_L)
+
+    u = lambda x: e[n](x)
+    for i in range(n):
+        u = union_f_g(u, lambda x: e[i](x) * a_u[i])
+
+    return u
+
--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from diff_eq import solve
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def main():
+    args = sys.argv[1]
+    print(solve(args[0]))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
