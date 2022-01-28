399cc75fabb518144820f4174549df00881da61f
Martyna Paliwoda <martynapaliwoda.2001@gmail.com>
2022-01-28 12:32:28 +0100

    code fixed

--- diff_eq.py
+++ diff_eq.py
@@ -1,59 +1,89 @@
 from numpy.linalg import inv
-from functools import partial
 from math import sin
 
 import scipy.integrate as integrate
 import numpy as np
 
-a = 0
-b = 2
-
 
 def B(u, v):
-    return v(b) * u(b) + integrate.quad(lambda x: -v(x) * u(x), a, b)[0]
+    a1, b1, a2, b2 = u[2], u[3], v[2], v[3]
+    if b1 >= a2 >= a1:
+        a = a2
+        b = b1
+    elif b2 >= a1 >= a2:
+        a = a1
+        b = b2
+    else:
+        return v[0](2) * u[0](2)
+    if a == b:
+        return v[0](2) * u[0](2)
+    return v[0](2) * u[0](2) - integrate.quad(lambda x: u[1](x) * v[1](x) - v[0](x) * u[0](x), a, b)[0]
 
 
 def L(v):
-    return integrate.quad(lambda x: sin(x) * v(x), a, b)[0]
+    a = v[2]
+    b = v[3]
+    return -integrate.quad(lambda x: sin(x) * v[0](x), a, b)[0]
 
 
-def f_linear(a, b):
-    return lambda x: a * x + b
+def e_i(i, n):
+    h = 2 / n
+    d1 = 1 - i
+    d2 = 1 + i
+
+    a = max(0, h * (i-1))
+    b = min(h * (i+1), 2)
+
+    def e(x):
+        if a <= x <= h * i:
+            return 1 / h * x + d1
+        elif h * i < x < b:
+            return -1 / h * x + d2
+        return 0
+
+    def de(x):
+        if a <= x <= h * i:
+            return 1 / h
+        elif h * i < x < b:
+            return -1 / h
+        return 0
+
+    return e, de, a, b
 
 
 def get_e(n):
     e = []
-    for i in range(1, n + 2):
-        a = n + 1
-        b = 1 - i
-        e.append(partial(f_linear(a, b)))
+    for i in range(n+1):
+        e.append(e_i(i, n))
 
     return e
 
 
-def get_arrays(B, L, e):
-    a_B = np.array([[B(u, v) for u in e] for v in e])
-    a_L = np.array([L(v) for v in e])
-
-    return a_B, a_L
-
-
-def get_a_u(a_B, a_L):
-    return inv(a_B).dot(a_L)
+def arrays(B, L, e):
+    arr_B = np.array([[B(u, v) for u in e] for v in e])
+    arr_L = np.array([L(v) for v in e])
+    return arr_B, arr_L
 
 
-def union_f_g(f, g):
-    return lambda x: f(x) + g(x)
+def dotArrays(arr_B, arr_L):
+    return inv(arr_B).dot(arr_L)
+
+
+def u(arr_u, e, x):
+    res = 0
+    for i in range(len(e)):
+        res += e[i][0](x) * arr_u[i]
+    return res
 
 
 def solve(n):
     e = get_e(n)
-    a_B, a_L = get_arrays(B, L, e[0:n])
-    a_u = get_a_u(a_B, a_L)
+    arr_B, arr_L = arrays(B, L, e)
+    arr_u = dotArrays(arr_B, arr_L)
 
-    u = e[n]
-    for i in range(n):
-        u = union_f_g(u, lambda x: e[i](x) * a_u[i])
+    return lambda x: u(arr_u, e, x)
 
-    return u
+
+
+
 
--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-import sys
-
+from matplotlib import pyplot as plt
 from diff_eq import solve
+from time import time
+import numpy as np
 
 
-def main():
-    args = sys.argv[1]
-    print(solve(args[0]))
+def draw_plot(n):
+    a = 0
+    b = 2
+    x_values = np.linspace(a, b)
+    y_values = list(map(solve(n), x_values))
+
+    plt.plot(x_values, y_values)
+    plt.show()
+
+
+n = 1000
+draw_plot(n)
 
